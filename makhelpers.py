204496004cd3a372ea24ab108edf759971e1e9fa
Mak Shimizu <mak@fish.razor.jp>
2019-06-11 23:47:58 +0900

    Update makhelpers.py

--- makhelpers.py
+++ makhelpers.py
@@ -1,2 +1,33 @@
-def test():
-  return 1
+def test(df):
+  # Userごとに連番をふる
+  df['Seq'] = df.groupby('GA ID').cumcount() + 1
+  
+  # 日本語のカラム名を英語に統一
+  d = {'ページの': 'Page',
+     'タイトル': ' title',
+     'イベント( |の)?': 'Event ',
+     'カテゴリ': 'category',
+     'アクション': 'action',
+     'ラベル': 'label',
+     '値': 'value',
+     '目標( |の)?': 'Goal ',
+     '完了数': 'completion(s)',
+     '番号': 'index',
+     '名': 'name',
+     '数': 'count',
+     # さらに読みやすくする
+     'sessions.channel': 'Channel',
+     'sessions.deviceCategory': 'Device'
+  }
+  df.columns = df.columns.to_series().replace(d, regex=True)
+  
+  # 配列を結合しフラット化
+  ar = ['Event action','Event category','Event label','Event count','Event value',
+      'Page URL','Page title',
+      'Product code','Product name','Product count','Product quantity','Product revenue','Transaction Id'
+     ]
+  for i in df2.columns.to_list():
+    if i in ar:
+        df2[i] = df2[i].apply(lambda x:",".join(x) if pd.notnull(x) else x)
+
+  return df
